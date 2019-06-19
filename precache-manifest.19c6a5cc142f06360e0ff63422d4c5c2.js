a93fd7a76efd98ee8f8caa5ba46a8d5061e2e049
loopy muchacha <lupegdealba@gmail.com>
2019-06-19 00:01:18 -0700

    Updates

---
+++ precache-manifest.19c6a5cc142f06360e0ff63422d4c5c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2569867d915caee79bc5bf6abc89b13",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "0feab3be6c1289d1910e",
+    "url": "/Clicky-Game/static/css/main.16616514.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "0feab3be6c1289d1910e",
+    "url": "/Clicky-Game/static/js/main.791a013f.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "b9e3e4e3def13c84198f3a1063a19f7a",
+    "url": "/Clicky-Game/static/media/picture10.b9e3e4e3.jpg"
+  },
+  {
+    "revision": "2011465d6523dc91997cee70d3a36d30",
+    "url": "/Clicky-Game/static/media/picture11.2011465d.jpg"
+  },
+  {
+    "revision": "5bccf71e544b920b1637d24ac9bf1e3d",
+    "url": "/Clicky-Game/static/media/picture12.5bccf71e.jpg"
+  },
+  {
+    "revision": "730d514f3319136ae3b4d47f0440dbee",
+    "url": "/Clicky-Game/static/media/picture7.730d514f.jpg"
+  },
+  {
+    "revision": "e134d0e8871f644d442a806f43f0d6c6",
+    "url": "/Clicky-Game/static/media/picture9.e134d0e8.jpg"
+  }
+]);
